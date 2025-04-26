941de93544167bb9b8a34fcc873f313fd5b98455
JongHyun070105 <bobi09361@gmail.com>
2025-04-26 22:01:10 +0900

    COMPLETE

---
+++ src/main/kotlin/day0426/PracticeProblems/Problem3_Gugudan.kt
@@ -0,0 +1,20 @@
+package day0426.PracticeProblems
+
+// 3. 구구단 출력 (특정 단)
+// 단 수를 변수로 선언하고, 해당 단의 구구단을 출력하는 프로그램을 작성하세요.
+
+fun main() {
+    val dan = 3  // 원하는 단 수를 설정하세요.
+
+    if(dan == 3){
+        println("3x1 = 3")
+        println("3x2 = 6")
+        println("3x3 = 9")
+        println("3x4 = 12")
+        println("3x5 = 15")
+        println("3x6 = 18")
+        println("3x7 = 21")
+        println("3x8 = 24")
+        println("3x9 = 27")
+    }
+}
