28bcee12969748c8f536fd1ca6e22697e2f29431
JongHyun070105 <bobi09361@gmail.com>
2025-04-27 21:51:07 +0900

    Learn WhileLoop

---
+++ src/main/kotlin/day0427/WhileLoop.kt
@@ -0,0 +1,15 @@
+package day0427
+
+fun main(){
+//    var i = 1
+//    while (i <=5){
+//        println(i)
+//        i++
+//    }
+
+    var count = 3
+    while(count > 0){
+        println("남은 횟수 $count")
+        count--
+    }
+}
