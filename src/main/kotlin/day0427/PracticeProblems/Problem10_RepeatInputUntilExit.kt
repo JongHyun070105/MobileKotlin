c533ba8c83b53df47f5c27ebffbabb5483483b99
JongHyun070105 <bobi09361@gmail.com>
2025-05-01 11:47:38 +0900

    COMPLETE

---
+++ src/main/kotlin/day0427/PracticeProblems/Problem10_RepeatInputUntilExit.kt
@@ -0,0 +1,12 @@
+package day0427.PracticeProblems
+
+// 문제 10. RepeatInputUntilExit
+// 사용자에게 "exit"이 입력될 때까지 계속해서 입력을 받는 프로그램을 작성하세요.
+
+fun main() {
+    var text : String
+    do {
+        text = readLine()!!.toString()  // 사용자 입력
+        // "exit"이 입력되면 종료
+    } while (text != "exit")
+}
