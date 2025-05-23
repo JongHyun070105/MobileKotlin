dfde3419241fca5b04d8215930ec1f32f04f7cb9
JongHyun070105 <bobi09361@gmail.com>
2025-05-23 22:16:54 +0900

    Learn Collection

---
+++ src/main/kotlin/LearnKotlinDay04/Collections.kt
@@ -0,0 +1,47 @@
+package LearnKotlinDay04
+
+fun main(){
+//    List
+//    // 불변형 List (읽기 전용)
+//    val fruits: List<String> = listOf("Apple", "Banana", "Cherry")
+//
+//    // 가변형 List (읽기/쓰기 가능)
+//    val numbers: MutableList<Int> = mutableListOf(1, 2, 3, 4, 5)
+//
+//    // List 요소 접근
+//    for(i in fruits){
+//        println("과일 :${i}")
+//    }
+//
+//    // List 크기 확인
+//    println("\nList의 크기는 ${fruits.size}")
+//
+//    // List 요소 추가(가변형만 가능)
+//    numbers.add(6)
+//    println(numbers)
+//
+//    // List 요소 제거(가변형만 가능)
+//    numbers.removeAt(0)
+//    println(numbers)
+
+//    Set
+//    // 불변형 Set 생성 (읽기 전용)
+//    val uniqueNumbers: Set<Int> = setOf(1, 2, 3, 4, 5, 5) // 중복값은 하나만 저장됨
+//
+//    // 가변형 set 생성 (읽기/쓰기 가능)
+//    val colors: MutableSet<String> = mutableSetOf("Red", "Orange", "Blue", "Blue")
+//
+//    // Set 요소 추가 (가변형만 가능)
+//    colors.add("Yellow")
+//    println(colors)
+//
+//    // Set 요소 제거 (가변형만 가능)
+//    colors.remove("Red")
+//    println(colors)
+//
+//    // Set에 특정 요소가 있는지 확인
+//    println(colors.contains("Green"))
+
+//    Map
+
+}
